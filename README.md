c857dba2394db99e7741ce1b9c7ac4277ec65f78
Marina Zhivotova <mazhivotova@gmail.com>
2025-03-23 22:58:47 +0200

    fix leaks in floodfill

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 42school_so_long
+<a href="https://www.freepik.com/free-vector/garden-template-element-game_2188412.htm">Image by brgfx on Freepik</a>
--- src/floodfill.c
+++ src/floodfill.c
@@ -6,7 +6,7 @@
 /*   By: mzhivoto <mzhivoto@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 12:22:08 by mzhivoto          #+#    #+#             */
-/*   Updated: 2025/03/23 15:15:57 by mzhivoto         ###   ########.fr       */
+/*   Updated: 2025/03/23 22:46:27 by mzhivoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 void	flood_fill(t_map *copy, int x, int y)
 {
-	printf("copy collet BEFORE x is %d\n", copy->collectables);
+	//printf("copy collet BEFORE x is %d\n", copy->collectables);
 	// printf("map size y is %d and y is %d\n", copy->size_y, y);
 	// printf("player position x %d and y %d\n", copy->player_pos.x, copy->player_pos.y);
-	int i = 0 ;
-	while (i < copy->size_y)
-	{
-		printf(" copy map area i %s\n", copy->area[i++]);
-	}
-	printf("***************************\n");
+	// int i = 0 ;
+	// while (i < copy->size_y)
+	// {
+	// 	printf(" copy map area i %s\n", copy->area[i++]);
+	// }
+	// printf("***************************\n");
 	if (copy->area[y][x] == 'C')
 	{
 		copy->collectables--;
-		printf("copy collet AFTER x is %d\n", copy->collectables);
+		//printf("copy collect AFTER x is %d\n", copy->collectables);
 	}
 		
 	if (copy->area[y][x] == 'E')
@@ -36,7 +36,7 @@ void	flood_fill(t_map *copy, int x, int y)
 	{
 		flood_fill(copy, x - 1, y);
 	}
-	if (x < copy->size_x && copy->area[y][x + 1] != '1')
+	if (x + 1 < copy->size_x && copy->area[y][x + 1] != '1')
 	{
 		flood_fill(copy, x + 1, y);
 	}
@@ -44,11 +44,10 @@ void	flood_fill(t_map *copy, int x, int y)
 	{
 		flood_fill(copy, x, y - 1);
 	}
-	if (y < copy->size_y && copy->area[y + 1][x] != '1')
+	if (y + 1 < copy->size_y && copy->area[y + 1][x] != '1')
 	{
 		flood_fill(copy, x, y + 1);
 	}
-	
 }
 
 int	path_check(t_map *map)
@@ -57,7 +56,7 @@ int	path_check(t_map *map)
 	int		i;
 
 	i = 0;
-	copy_map = ft_calloc(1, sizeof(t_map *));
+	copy_map = ft_calloc(1, sizeof(t_map));
 	if (!copy_map)
 		return (free_map(map), error_msg("memory error"), -1);
 	copy_map->collectables = map->collectables;
@@ -67,18 +66,38 @@ int	path_check(t_map *map)
 	copy_map->player_pos.y = map->player_pos.y;
 	copy_map->exit_pos.x = map->exit_pos.x;
 	printf("copy map player position x %d and y %d\n", copy_map->player_pos.x, copy_map->player_pos.y);
-	copy_map->area = ft_calloc(copy_map->size_y, sizeof(char *));
+	copy_map->area = ft_calloc(copy_map->size_y + 1, sizeof(char *)); 
 	if (!copy_map->area)
 		return (free_map(map), error_msg("memory error"), -1);
 	while (i < copy_map->size_y)
 	{
 		copy_map->area[i] = ft_strdup(map->area[i]);
+		 if (!copy_map->area[i])
+            return (free_map(copy_map), error_msg("memory error"), -1);
 		i++;
 	}
-	flood_fill(copy_map, copy_map->player_pos.y, copy_map->player_pos.x);
+	flood_fill(copy_map, copy_map->player_pos.x, copy_map->player_pos.y);
 	printf("copy collet FINAL x is %d\n", copy_map->collectables);
-	if (copy_map->collectables > 0 || copy_map->exit_pos.x > 0)
+	if (copy_map->collectables > 0 || copy_map->exit_pos.x != -1)
 		return (free_map(copy_map), error_msg("no valid path found"), -1);
 	free_map(copy_map);
 	return (1);
 }
+// if (x > 0 && copy->area[y][x - 1] != '1')
+// 	{
+// 		flood_fill(copy, x - 1, y);
+// 	}
+// 	if (x < copy->size_x && copy->area[y][x + 1] != '1')
+// 	x + 1 < (int)ft_strlen(copy->area[y]) && copy->area[y][x + 1] != '1'
+// 	{
+// 		flood_fill(copy, x + 1, y);
+// 	}
+// 	if (y > 0 && copy->area[y - 1][x] != '1')
+// 	{
+// 		flood_fill(copy, x, y - 1);
+// 	}
+// 	if (y < copy->size_y && copy->area[y + 1][x] != '1')
+// 	y + 1 < copy->size_y && copy->area[y + 1][x] != '1'
+// 	{
+// 		flood_fill(copy, x, y + 1);
+// 	}
